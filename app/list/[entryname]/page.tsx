4245c8abe33db52c43b785aa6308fbb749997cd6
teoMarinov <teodor.b.marinov@gmail.com>
2024-02-27 03:07:09 +0200

    remove repeating code from /list

--- app/list/[entryname]/page.tsx
+++ app/list/[entryname]/page.tsx
@@ -1,8 +1,10 @@
 "use client";
+import MobileDataTableRow from "@/app/components/DataTable/MobileDataTableRow";
+import TableHeaderComponent from "@/app/components/DataTable/TableHeaderComponent";
+import TableRowComponent from "@/app/components/DataTable/TableRowComponent";
+import { EntryType } from "@/app/types";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import DesktopDataTable from "../components/DesktopDataTable";
-import MobileDataTable from "../components/MobileDataTable";
 
 interface SearchListProps {
   params: {
@@ -10,7 +12,53 @@ interface SearchListProps {
   };
 }
 const SearchList: React.FC<SearchListProps> = ({ params }) => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<EntryType[]>([]);
+  const [orderBy, setOrderBy] = useState("Date");
+  const [isAscending, setIsAscending] = useState(true);
+
+  const sortBy = (field: string) => {
+    if (orderBy === field) {
+      setData(data.reverse());
+      setIsAscending(!isAscending);
+    } else {
+      setOrderBy(field);
+      setIsAscending(true);
+      switch (field) {
+        case "Name":
+          return sortByName();
+        case "Date":
+          return sortByDate();
+        case "Difficulty":
+          return sortByDifficulty();
+      }
+    }
+  };
+
+  const sortByName = () => {
+    const sortedArr = data.sort((a: any, b: any) => {
+      return a.name.localeCompare(b.name);
+    });
+    setData(sortedArr);
+  };
+
+  const sortByDifficulty = () => {
+    const sortedArr = data.sort((a: EntryType, b: EntryType) => {
+      const difficultyOrder: any = {
+        Easy: 0,
+        Medium: 1,
+        Hard: 2,
+      };
+      return difficultyOrder[b.difficulty] - difficultyOrder[a.difficulty];
+    });
+    setData(sortedArr);
+  };
+
+  const sortByDate = () => {
+    const sortedArr = data.sort((a: EntryType, b: EntryType) => {
+      return Number(new Date(a.createdAt)) - Number(new Date(b.createdAt));
+    });
+    setData(sortedArr);
+  };
 
   useEffect(() => {
     axios
@@ -20,8 +68,44 @@ const SearchList: React.FC<SearchListProps> = ({ params }) => {
 
   return (
     <>
-      <DesktopDataTable dataProp={data} />
-      <MobileDataTable dataProp={data} />
+      {/* Desktop data table */}
+      <>
+        <div className="mt-3 border-t hidden xl:block">
+          <TableHeaderComponent
+            orderBy={orderBy}
+            ascending={isAscending}
+            sortBy={sortBy}
+          >
+            {data.map((entry: EntryType) => (
+              <TableRowComponent
+                key={entry.id}
+                name={entry.name}
+                link={entry.urlTo}
+                difficulty={entry.difficulty}
+                date={entry.createdAt}
+                code={entry.code}
+                description={entry.description}
+              />
+            ))}
+          </TableHeaderComponent>
+        </div>
+      </>
+      {/* Mobile data table */}
+      <>
+        <div className=" xl:hidden ">
+          {data.map((entry: EntryType) => (
+            <MobileDataTableRow
+              key={entry.id}
+              name={entry.name}
+              link={entry.urlTo}
+              difficulty={entry.difficulty}
+              date={entry.createdAt}
+              code={entry.code}
+              description={entry.description}
+            />
+          ))}
+        </div>
+      </>
     </>
   );
 };
--- app/list/page.tsx
+++ app/list/page.tsx
@@ -2,11 +2,59 @@
 
 import axios from "axios";
 import { useState, useEffect } from "react";
-import DesktopDataTable from "./components/DesktopDataTable";
-import MobileDataTable from "./components/MobileDataTable";
+import { EntryType } from "../types";
+import TableHeaderComponent from "../components/DataTable/TableHeaderComponent";
+import TableRowComponent from "../components/DataTable/TableRowComponent";
+import MobileDataTableRow from "../components/DataTable/MobileDataTableRow";
 
 const ListEntries = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<EntryType[]>([]);
+  const [orderBy, setOrderBy] = useState("Date");
+  const [isAscending, setIsAscending] = useState(true);
+
+  const sortBy = (field: string) => {
+    if (orderBy === field) {
+      setData(data.reverse());
+      setIsAscending(!isAscending);
+    } else {
+      setOrderBy(field);
+      setIsAscending(true);
+      switch (field) {
+        case "Name":
+          return sortByName();
+        case "Date":
+          return sortByDate();
+        case "Difficulty":
+          return sortByDifficulty();
+      }
+    }
+  };
+
+  const sortByName = () => {
+    const sortedArr = data.sort((a: any, b: any) => {
+      return a.name.localeCompare(b.name);
+    });
+    setData(sortedArr);
+  };
+
+  const sortByDifficulty = () => {
+    const sortedArr = data.sort((a: EntryType, b: EntryType) => {
+      const difficultyOrder: any = {
+        Easy: 0,
+        Medium: 1,
+        Hard: 2,
+      };
+      return difficultyOrder[b.difficulty] - difficultyOrder[a.difficulty];
+    });
+    setData(sortedArr);
+  };
+
+  const sortByDate = () => {
+    const sortedArr = data.sort((a: EntryType, b: EntryType) => {
+      return Number(new Date(a.createdAt)) - Number(new Date(b.createdAt));
+    });
+    setData(sortedArr);
+  };
 
   useEffect(() => {
     axios.get("/api/getentries").then((response) => {
@@ -16,8 +64,44 @@ const ListEntries = () => {
 
   return (
     <>
-      <DesktopDataTable dataProp={data} />
-      <MobileDataTable dataProp={data} />
+        {/* Desktop data table */}
+      <>
+        <div className="mt-3 border-t hidden xl:block">
+          <TableHeaderComponent
+            orderBy={orderBy}
+            ascending={isAscending}
+            sortBy={sortBy}
+          >
+            {data.map((entry: EntryType) => (
+              <TableRowComponent
+                key={entry.id}
+                name={entry.name}
+                link={entry.urlTo}
+                difficulty={entry.difficulty}
+                date={entry.createdAt}
+                code={entry.code}
+                description={entry.description}
+              />
+            ))}
+          </TableHeaderComponent>
+        </div>
+      </>
+      {/* Mobile data table */}
+      <>
+        <div className=" xl:hidden ">
+          {data.map((entry: EntryType) => (
+            <MobileDataTableRow
+              key={entry.id}
+              name={entry.name}
+              link={entry.urlTo}
+              difficulty={entry.difficulty}
+              date={entry.createdAt}
+              code={entry.code}
+              description={entry.description}
+            />
+          ))}
+        </div>
+      </>
     </>
   );
 };
